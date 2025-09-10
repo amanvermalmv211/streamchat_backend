1632b7d644b7513c91b3412ccdd050490450594b
amanvermalmv211 <amanvermalmv211@gmail.com>
2025-09-10 12:04:20 +0530

    Changes in secure

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -65,12 +65,12 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
     try {
-        res.clearCookie("jwt", "", {
-            maxAge: 0,
+        res.clearCookie("jwt", {
             httpOnly: true,
             secure: true,          // must match cookie when set
             sameSite: "none",      // important for cross-origin
-            path: "/"
+            path: "/",
+            domain: "streamchat-backend-lt2n.onrender.com"
         });
         res.status(200).json({ message: "User log out successfully" });
     } catch (error) {
